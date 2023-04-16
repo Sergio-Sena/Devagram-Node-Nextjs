237c6a2268a7faac4bcc99ce1d413acd1e263273
Sergio Pereira Sena <senanetworker@gmail.com>
2023-04-16 12:29:55 -0300

    API usuario encontrado

--- pages/api/pesquisa.ts
+++ pages/api/pesquisa.ts
@@ -3,21 +3,34 @@ import type { respostaPadraoMsg } from '../../types/respostaPadraoMsg';
 import { conectarMongoDB } from '@/middlewares/conectarMongoDB';
 import { validarTokenJWT } from '@/middlewares/validarTokenJWT';
 import { UsuarioModel } from '@/models/UsuarioModel';
+import usuario from './usuario';
 
 
 const pesquisaEndpoint = async (req: NextApiRequest, res: NextApiResponse<respostaPadraoMsg | any[]>) => {
     try {
         if (req.method === 'GET') {
-            const { filtro } = req.query;
-            if (!filtro || filtro.length < 2) {
-                return res.status(400).json({ erro: 'Favor informar um filtro valido!' })
-            }
-            const usuarioEncontrados = await UsuarioModel.find({
-                $or: [{ nome: { $regex: filtro, $options: 'i' } },
-                { email: { $regex: filtro, $options: 'i' } }]
-            })
-            return res.status(200).json(usuarioEncontrados);
+            if (req?.query?.id) {
+                const usuarioEncontrados = await UsuarioModel.findById(req?.query?.id);
+                if(!usuarioEncontrados){
+                    return res.status(400).json({erro:'Usuario nao encontrado.'})
+                }else{
+                    usuarioEncontrados.senha = null
+                    return res.status(200).json(usuarioEncontrados)
+                }
 
+
+            } else {
+                const { filtro } = req.query;
+                if (!filtro || filtro.length < 2) {
+                    return res.status(400).json({ erro: 'Favor informar um filtro valido!' })
+                }
+                const usuarioEncontrados = await UsuarioModel.find({
+                    $or: [{ nome: { $regex: filtro, $options: 'i' } },
+                    { email: { $regex: filtro, $options: 'i' } }]
+                })
+                return res.status(200).json(usuarioEncontrados);
+
+            }
         }
 
     } catch (e) {
